ae84a1509c30cfd5c16930ab5c8e3cb602ce5156
Lennart Steffen <lennart.steffen@tu-berlin.de>
2022-08-15 18:28:40 +0200

    extended example

--- example/example.cpp
+++ example/example.cpp
@@ -1,7 +1,15 @@
 #include "gnuplot.h"
+#include <chrono>
+#include <thread>
+
 
 int main(){
     GnuplotPipe gp;
     gp.sendLine("plot [-pi/2:pi] cos(x),-(sin(x) > sin(x+1) ? sin(x) : sin(x+1))");
+    gp.sendEndOfData();
+    int duration {2};
+    std::cout << "Sleeping for " << duration << "s, then plotting next graph.\n";
+    std::this_thread::sleep_for(std::chrono::seconds(duration));
+    gp.sendLine("plot [-pi/2:pi] -(sin(x) > sin(x+1) ? sin(x+1) : sin(x))");
     return 0;
 }
